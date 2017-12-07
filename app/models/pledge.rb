63cb1c3ead7c2cfcc217b26aa278107b7ab15edd
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-12-07 18:51:49 -0500

    corrected initial testings

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,4 +4,13 @@ class Pledge < ApplicationRecord
 
   validates :dollar_amount, presence: true
   validates :user, presence: true
+  validate :owner_cannot_back_own_project
+
+  def owner_cannot_back_own_project
+    if user ==
+      self.project.user
+      errors.add(:user, 'Owner should not be able to pledge towards own project')
+    end
+  end
+
 end
