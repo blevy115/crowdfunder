c3cbd82e0aa3edbebeaf7df5ba6797a2c9ffc9c2
cordell <cordell.perry@gmail.com>
2017-12-09 23:13:35 -0500

    project validation for start and end date(not tested)

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,19 @@ class Project < ActiveRecord::Base
   belongs_to :user # project owner
 
   validates :user, :title, :description, :goal, :start_date, :end_date, presence: true
+  validate :before_start_date_after_start_date
+
+  def before_start_date_after_start_date
+  if :start_date > current_time && :end_date > :start_date
+  else
+    project.errors.add(:start_date, :end_date,"date needs to be in the future", "date must be later then end date ")
+  end
+
+
+
+  private
+
+  def current_time
+    @current_time = Time.now
+  end
 end
