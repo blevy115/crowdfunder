5b58cd45ade00bc5210e24201fa71f60aafb0814
cordell <cordell.perry@gmail.com>
2017-12-12 16:33:32 -0500

    refactor pledge with factory bot

--- test/models/reward_test.rb
+++ test/models/reward_test.rb
@@ -2,60 +2,35 @@ require 'test_helper'
 
 class RewardTest < ActiveSupport::TestCase
 
+  setup do
+    @project = create(:project)
+    @reward = build(:reward, project: @project)
+  end
+
   test 'A reward can be created' do
-    project = new_project
-    project.save
-    reward = Reward.create(
-      dollar_amount: 99.00,
-      description: 'A heartfelt thanks!',
-      project: project
-    )
-    assert reward.valid?
-    assert reward.persisted?
+    @reward.save
+     assert @reward.valid?
+     assert @reward.persisted?
   end
 
   test 'A reward cannot be created without a dollar amount' do
-    project = new_project
-    project.save
-    reward = Reward.create(
-      description: 'A heartfelt thanks!',
-      project: project
-    )
-    assert reward.invalid?, 'Reward should be invalid without dollar amount'
-    assert reward.new_record?, 'Reward should not save without dollar amount'
+    @reward.dollar_amount = nil
+     @reward.save
+     assert @reward.invalid?, 'Reward should be invalid without dollar amount'
+     assert @reward.new_record?, 'Reward should not save without dollar amount'
   end
 
   test 'A reward cannot be created without a description' do
-    project = new_project
-    project.save
-    reward = Reward.create(
-      dollar_amount: 99.00,
-      project: project
-    )
-    assert reward.invalid?, 'Reward should be invalid without a description'
-    assert reward.new_record?, 'Reward should not save without a description'
+     @reward.description = nil
+     @reward.save
+     assert @reward.invalid?, 'Reward should be invalid without a description'
+     assert @reward.new_record?, 'Reward should not save without a description'
   end
 
-  def test_reward_positive
-    project = new_project
-    project.save
-    reward = Reward.create(
-      dollar_amount: 0,
-      description: 'A heartfelt thanks!',
-      project: project
-    )
-    assert reward.invalid?
-  end
-
-
-  def new_project
-    Project.new(
-      title:       'Cool new boardgame',
-      description: 'Trade sheep',
-      start_date:  Date.today,
-      end_date:    Date.today + 1.month,
-      goal:        50000
-    )
+  test 'reward is positive' do
+   @reward.dollar_amount = 0
+   @reward.save
+   assert @reward.invalid?
   end
 
 end
