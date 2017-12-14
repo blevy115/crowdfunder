830f7409ab73d9ac91803b901fd5d73b60a96711
co2co2 <coco8287@hotmail.com>
2017-12-14 12:38:11 -0500

    cut current user method from appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,5 @@ class ApplicationController < ActionController::Base
     redirect_to login_path, alert: "Please login first"
   end
 
-  def current_user
-  @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
 
-  helper_method :current_user
 end
